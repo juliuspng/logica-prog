c9b55080aa5f11d1450de6e1a15ede5b38d16437
juliucesarALRN <julius.cesar.lula.silva@gmail.com>
2023-11-01 14:02:31 -0300

    questão incompleta

--- q14.py
+++ q14.py
@@ -5,3 +5,11 @@ print("Hello world!")
 print("Hello", "world")
 
 # item c
+print("Hello!\nMy name is John")
+
+# item d
+print("Hello!\tMy name is John")
+
+# item e
+print("Um dia, ela disse: \"Estarei aqui por você.\"")
+      
